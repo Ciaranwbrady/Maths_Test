1feff81c706252789b0dac41286cb1a019858611
Ciaranwbrady <ciaranwbrady@gmail.com>
2021-02-23 14:26:38 +0000

    Add listener event for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+//Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them. 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click",function() {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+function runGame(){
+    // Generate Two random numbers between 1 and 25
+    // Math.flo0r rounds down to the whole number
+    // Math.random generates random numbers
+    
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
